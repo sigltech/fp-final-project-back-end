324788202b2059cb0d78bfb7ff4384d8b340f59f
sigltech <101203107+sigltech@users.noreply.github.com>
2022-08-08 12:17:34 +0100

    heroku changes

--- server_app/routes/main.py
+++ server_app/routes/main.py
@@ -5,17 +5,17 @@ from ..models.tables import Clothing
 main_routes = Blueprint("main", __name__)
 
 
-@main_routes.route("/")
-def index():
-    return jsonify({"message": "Hello World!"}) 
-
-# @main_routes.route("/", methods=["GET"])
+# @main_routes.route("/")
 # def index():
-#     if request.method == "GET":
-#         all_clothing = Clothing.query.all()
-#         return jsonify(all_clothing)
-#     else: 
-#         pass
+#     return jsonify({"message": "Hello World!"}) 
+
+@main_routes.route("/", methods=["GET"])
+def index():
+    if request.method == "GET":
+        all_clothing = Clothing.query.all()
+        return jsonify(all_clothing)
+    else: 
+        pass
 
 # @main_routes.route("/clothing")
 # def clothing():
